07e6a223c28546c621609efb1e3d3b78cb748546
a.qaiss <a.qaiss@isicod.net>
2023-11-09 21:32:43 +0100

    adding sidebar web and mobile + input search

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -7,6 +7,7 @@ import { Poppins } from "next/font/google";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import { ModeToggle } from "./theme-toggle";
+import MobileSidebar from "./mobileSidebar";
 
 
 const font = Poppins({
@@ -16,9 +17,9 @@ const font = Poppins({
 
 function NavBar() {
     return (
-        <div className="fixed w-full z-50 flex justify-between items-center py-2 px-4 border-b border-primary/10 bg-secondary">
+        <div className="fixed w-full z-50 flex justify-between items-center py-3 px-4 border-b border-primary/10 bg-secondary">
             <div className="flex items-center">
-                <Menu className="block md:hidden" />
+                <MobileSidebar />
                 <Link href="/">
                     <h1 className={cn("hidden md:block text-xl md:text-3xl font-bold text-primary",
                         font.className)}>companion.ai</h1>
