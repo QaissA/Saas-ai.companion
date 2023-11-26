515a26218e69f162bf25d8797eaf5f412fe92370
QaissA <qaissabdelhamid@gmail.com>
2023-11-26 20:32:04 +0100

    fix

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -25,7 +25,7 @@ function NavBar({ isPro }: NavBarProps) {
     return (
         <div className="fixed w-full z-50 flex justify-between items-center py-3 px-4 border-b border-primary/10 bg-secondary">
             <div className="flex items-center">
-                <MobileSidebar />
+                <MobileSidebar isPro={isPro} />
                 <Link href="/">
                     <h1 className={cn("hidden md:block text-xl md:text-3xl font-bold text-primary",
                         font.className)}>companion.ai</h1>
